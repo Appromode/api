ca94b6686426cdff6bcd8240df17ede649bec1fd
Appromode <steve2918parkinson@gmail.com>
2021-12-31 16:46:31 +0000

    TagDTO

---
+++ marking-api.DataModel/DTOs/TagDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marking_api.DataModel.DTOs
+{
+    public class TagDTO
+    {
+        public string TagName { get; set; }
+    }
+}
