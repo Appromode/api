11a0ea5e919c07cc83054ef02b15b4118b6e7752
Steven Parkinson <steve2917parkinson@hotmail.co.uk>
2022-02-01 17:11:30 +0000

    Add missing characters

--- marking-api.DataModel/Identity/User.cs
+++ marking-api.DataModel/Identity/User.cs
@@ -13,7 +13,7 @@ namespace marking_api.DataModel.Identity
         [PersonalData]
         public bool IsDisabled { get; set; }
         public bool IsDeleted { get; set; }
-        public byte[] ProfilePicture { get; set; } = Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAADIAAAAyBAMAAADsEZWCAAAAElBMVEXR0dGqqqrCwsKysrK5ubnKysqGpJ8RAAAAtUlEQVQ4y72TMQ7CMAxFTaAH + CnsKRI7QepOBvZWgvtfBaESSv1jKRKob32K / W238iOP40lKuATAC9NEvLiy6TBxoSdvwa + 2WVCrER+USbMJSxNhNcLMeW3D2UwTzEkHWnXG3FsrtdVg760qdW22nXltSRSNyolmVIPyZ+XJNOo6HGEQo9xeLOP/ZDZmH9t0VmqHiZ5WkNThWKBdKBfxxa1wUP3n34GycmD6vH4m5CbEIc/OyBPDUSvwZuB80QAAAABJRU5ErkJggg");
+        public byte[] ProfilePicture { get; set; } = Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAADIAAAAyBAMAAADsEZWCAAAAElBMVEXR0dGqqqrCwsKysrK5ubnKysqGpJ8RAAAAtUlEQVQ4y72TMQ7CMAxFTaAH+CnsKRI7QepOBvZWgvtfBaESSv1jKRKob32K/W238iOP40lKuATAC9NEvLiy6TBxoSdvwa+2WVCrER+USbMJSxNhNcLMeW3D2UwTzEkHWnXG3FsrtdVg760qdW22nXltSRSNyolmVIPyZ+XJNOo6HGEQo9xeLOP/ZDZmH9t0VmqHiZ5WkNThWKBdKBfxxa1wUP3n34GycmD6vH4m5CbEIc/OyBPDUSvwZuB80QAAAABJRU5ErkJggg==");
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
