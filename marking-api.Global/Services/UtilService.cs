48d8dd7972d076830deb916ed2546eb3c8182155
Steven Parkinson <steven@maly.co.uk>
2022-02-14 12:29:42 +0000

    Login endpoint changes

--- marking-api.Global/Services/UtilService.cs
+++ marking-api.Global/Services/UtilService.cs
@@ -1,4 +1,6 @@
 ﻿using marking_api.DataModel.Identity;
+using marking_api.Global.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +11,26 @@ namespace marking_api.Global.Services
 {
     public class UtilService
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public UtilService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public bool IsDateTimeNullOrEmpty(DateTime? date)
         {
             return !date.HasValue ? true : false;
         }
 
-        public List<LinkDM> GenerateUserMenu()
+        public List<LinkDM> GenerateUserMenu(string userId)
         {
-            return null;
+            //List<LinkDM> userLinks = _unitOfWork.Links.Get(include: x => x.Include());
+            return null; //Generate nav menu for user
         }
 
-        public void RecurseChildLinks()
+        public void RecurseChildLinks(List<LinkDM> userLinks, Int64 ParentLinkId, LinkDM ParentLink)
         {
-
+            //Recurse child links per parent link
         }
     }
 }
