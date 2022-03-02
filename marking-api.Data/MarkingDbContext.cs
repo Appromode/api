19b3bc335fa74a4270d713ed110dc32e44cc87d8
Alexander Watts <alexanderwatts@stu848F.kent.ac.uk>
2022-03-02 17:16:09 +0000

    Added user tags to marking db context

--- marking-api.Data/MarkingDbContext.cs
+++ marking-api.Data/MarkingDbContext.cs
@@ -65,6 +65,7 @@ namespace marking_api.Data
         public DbSet<ThreadDM> Threads { get; set; }
         public DbSet<UserGradeDM> UserGrades { get; set; }
         public DbSet<UserGroupDM> UserGroups { get; set; }
+        public DbSet<UserTagsDM> UserTags { get; set; }
 
         //API
         public DbSet<RefreshTokenDM> RefreshTokens { get; set; }
