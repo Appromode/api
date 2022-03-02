86bae471fbe977ba94101d6ba8d71efe0014be2d
Alexander Watts <alexanderwatts@stu848F.kent.ac.uk>
2022-03-02 17:17:12 +0000

    Added user tags migrations

---
+++ marking-api.Data/Migrations/20220302170144_UserTags.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace marking_api.Data.Migrations
+{
+    public partial class UserTags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserTags",
+                schema: "dbo",
+                columns: table => new
+                {
+                    UserTagId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TagId = table.Column<long>(type: "bigint", nullable: false),
+                    canDelete = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    deleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    deletedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    AccessRole = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTags", x => x.UserTagId);
+                    table.ForeignKey(
+                        name: "FK_UserTags_IdUsers_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "dbo",
+                        principalTable: "IdUsers",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalSchema: "dbo",
+                        principalTable: "Tags",
+                        principalColumn: "TagId",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTags_TagId",
+                schema: "dbo",
+                table: "UserTags",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTags_UserId",
+                schema: "dbo",
+                table: "UserTags",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserTags",
+                schema: "dbo");
+        }
+    }
+}
