d08f8b5bd64e16e891ca13f38662aa2d3d8d1287
Alexander Watts <alexanderwatts@eduD9E5.kent.ac.uk>
2022-03-20 23:42:56 +0000

    Updated imports recommend cm

--- marking-api.API/Models/Recommend/RecommendCM.cs
+++ marking-api.API/Models/Recommend/RecommendCM.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
-using marking_api.DataModel.Identity;
-using marking_api.DataModel.Project;
 using marking_api.Global.Repositories;
 using Microsoft.EntityFrameworkCore;
 using marking_api.DataModel.DTOs;
+using marking_api.DataModel.Identity;
 using System.Linq;
 
 namespace marking_api.API.Models.Recommend
