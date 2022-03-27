94a7c77beeac622f3e4b533bb03c838029db1a37
Steven Parkinson <steve2917parkinson@hotmail.co.uk>
2022-03-27 16:15:04 +0100

    Seed roles

--- marking-api.Data/MarkingDbSeeder.cs
+++ marking-api.Data/MarkingDbSeeder.cs
@@ -74,6 +74,8 @@ namespace marking_api.Data
                 _dbContext.SaveChanges();
 
                 SeedUsers();
+                SeedLinks();
+                SeedRoles(user);
             }
         }
 
@@ -158,6 +160,161 @@ namespace marking_api.Data
             };
             user3.PasswordHash = _signInManager.UserManager.PasswordHasher.HashPassword(user3, "Tester!2");
             _dbContext.Users.Add(user3);
+            _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Seed menu links into the database on initial database creation
+        /// </summary>
+        public void SeedLinks()
+        {
+            var link = new LinkDM
+            {
+                LinkName = "Project Forum",
+                LinkUrl = "/project-forum",
+                LinkPosition = 0,
+                LinkSecurity = "project forum"
+            };
+            _dbContext.Links.Add(link);
+            
+
+            link = new LinkDM
+            {
+                LinkName = "Project Supervisors",
+                LinkUrl = "/project-supervisors",
+                LinkPosition = 1,
+                LinkSecurity = "project security"
+            };
+            _dbContext.Links.Add(link);
+
+            link = new LinkDM
+            {
+                LinkName = "Dashboard",
+                LinkUrl = "/dashboard",
+                LinkPosition = 2,
+                LinkSecurity = "dashboard"
+            };
+            _dbContext.Links.Add(link);
+
+            var config = new LinkDM
+            {
+                LinkName = "Config",
+                LinkPosition = 3,
+            };
+            _dbContext.Links.Add(config);
+            _dbContext.SaveChanges();
+
+            link = new LinkDM
+            {
+                LinkName = "Query",
+                LinkUrl = "/config/query",
+                LinkPosition = 0,
+                LinkSecurity = "query",
+                LinkParentId = config.LinkId
+            };
+            _dbContext.Links.Add(link);
+
+            link = new LinkDM
+            {
+                LinkName = "Users",
+                LinkUrl = "/config/users",
+                LinkPosition = 1,
+                LinkSecurity = "users",
+                LinkParentId = config.LinkId
+            };
+            _dbContext.Links.Add(link);
+
+            link = new LinkDM
+            {
+                LinkName = "Roles",
+                LinkUrl = "/config/roles",
+                LinkPosition = 2,
+                LinkSecurity = "roles",
+                LinkParentId = config.LinkId
+            };
+            _dbContext.Links.Add(link);
+            _dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Seed roles and role links into the database
+        /// </summary>
+        public void SeedRoles(User user)
+        {
+            var role = new Role
+            {
+                Name = "Guest",
+                NormalizedName = "GUEST",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                RoleDescription = "Guest Role"
+            };
+            _dbContext.Roles.Add(role);
+
+            var student = new Role
+            {
+                Name = "Student",
+                NormalizedName = "STUDENT",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                RoleDescription = "Student Role"
+            };
+            _dbContext.Roles.Add(student);
+
+            role = new Role
+            {
+                Name = "Lecturer",
+                NormalizedName = "LECTURER",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                RoleDescription = "Lecturer Role"
+            };
+            _dbContext.Roles.Add(role);
+
+            var admin = new Role
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                RoleDescription = "Admin Role"
+            };
+            _dbContext.Roles.Add(admin);
+            _dbContext.SaveChanges();
+
+            var userrole = new UserRole
+            {
+                RoleId = admin.Id,
+                UserId = user.Id
+            };
+            _dbContext.UserRoles.Add(userrole);
+
+            List<User> users = _dbContext.Users.Where(x => x.Email != "sjp75@kent.ac.uk").ToList();
+            foreach (var testuser in users)
+            {
+                userrole = new UserRole
+                {
+                    RoleId = student.Id,
+                    UserId = testuser.Id
+                };
+            }
+            _dbContext.SaveChanges();
+
+            List<LinkDM> links = _dbContext.Links.ToList();
+
+            foreach (var link in links)
+            {
+                _dbContext.RoleLinks.Add(new RoleLinkDM
+                {
+                    LinkId = link.LinkId,
+                    RoleId = admin.Id
+                });
+                if (!link.LinkUrl.Contains("config"))
+                {
+                    _dbContext.RoleLinks.Add(new RoleLinkDM
+                    {
+                        LinkId = link.LinkId,
+                        RoleId = student.Id
+                    });
+                }
+                
+            }
             _dbContext.SaveChanges();
         }
     }
