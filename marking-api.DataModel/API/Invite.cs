dcbcac600a591196d66da20feacbb29c06076c8d
Alexander Watts <alexanderwatts@eduD9E5.kent.ac.uk>
2022-03-30 19:51:41 +0100

    Renamed accept invite request to invite

--- marking-api.DataModel/API/Invite.cs
+++ marking-api.DataModel/API/Invite.cs
@@ -6,7 +6,7 @@ namespace marking_api.DataModel.API
     /// <summary>
     /// Accept invite request class. Used to get post data when a user accepts an invite.
     /// </summary>
-    public class AcceptInvite
+    public class Invite
     {
         /// <summary>
         /// Id of invite
