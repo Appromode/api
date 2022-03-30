695efd080315ffec8ea148e3d5e5a3418d8107f1
Alexander Watts <alexanderwatts@eduD9E5.kent.ac.uk>
2022-03-30 19:17:24 +0100

    Removed comments

--- marking-api.DataModel/API/AddTagRequest.cs
+++ marking-api.DataModel/API/AddTagRequest.cs
@@ -4,14 +4,8 @@ using System;
 
 namespace marking_api.DataModel.API
 {
-    /// <summary>
-    /// Accept invite request class. Used to get post data when a user accepts an invite.
-    /// </summary>
     public class AddTagRequest
     {
-        /// <summary>
-        /// Id of invite
-        /// </summary>
         public string UserId { get; set; }
         public List<TagDM> tags { get; set; }
     }
